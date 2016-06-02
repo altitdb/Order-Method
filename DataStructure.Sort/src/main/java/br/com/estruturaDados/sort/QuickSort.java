732ad0c36cf0ea42471a4210535ae3db5bfaa79d
Leonardo Andriotti <leonardoandriotte@hotmail.com>
2016-06-02 14:29:30 -0300

    update

--- DataStructure.Sort/src/main/java/br/com/estruturaDados/sort/QuickSort.java
+++ DataStructure.Sort/src/main/java/br/com/estruturaDados/sort/QuickSort.java
@@ -1,11 +1,27 @@
 package br.com.estruturaDados.sort;
 
+import java.util.Arrays;
+
 import br.com.factoryPattern.Factory;
+import br.com.factoryPattern.WriteFactory;
 
 public class QuickSort implements Factory {
+	public long[]time = new long[6];
+	int cont=0;
 
 	public int[] structure(int[] value) {
+		long start_time = System.currentTimeMillis();
 		new QuickSort().ordenar(value, 0, value.length - 1);
+		long end_time = System.currentTimeMillis();
+
+		long difference = end_time-start_time;
+		
+		time[cont]=difference;
+		cont++;
+		System.out.println("tempo de execução"+difference);
+		System.out.println(cont +"cont");
+		System.out.println(Arrays.toString(time) +" atual tempo");
+		
 		return value;
 	}
 
@@ -36,6 +52,11 @@ public class QuickSort implements Factory {
 		vetor[inicio] = vetor[fim];
 		vetor[fim] = pivo;
 		return fim;
+	}
+	
+	public void write(){
+		WriteFactory write = new WriteFactory();
+		write.transform(time);
 	}
 
 }
--- DataStructure.Sort/src/main/java/br/com/factoryPattern/ReadFactory.java
+++ DataStructure.Sort/src/main/java/br/com/factoryPattern/ReadFactory.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Scanner;
 
 import br.com.estruturaDados.sort.BubbleSort;
+import br.com.estruturaDados.sort.InsertionSort;
+import br.com.estruturaDados.sort.QuickSort;
+import br.com.estruturaDados.sort.SelectionSort;
 
 public class ReadFactory {
 	public String[] read(String path) {
@@ -35,21 +37,61 @@ public class ReadFactory {
 	}
 
 	public static void main(String[] args) throws IOException {
-		BubbleSort bubble = new BubbleSort();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("informe Qual arquivo deseja calcular? (orderly,disorderly,orderReverse,percentageLeft,percentageRigth)");
+		System.out
+				.println("Qual estrutura de dados deseja ? 1-BubbleSort/2-SelectionSort/3-InsertSort/4-QuickSort");
+		int input = scanner.nextInt();
+		System.out
+				.println("informe Qual arquivo deseja calcular? (orderly,disorderly,orderReverse,percentageLeft,percentageRigth)");
 		String value = scanner.next();
-		for(int i =1; i<=5;i++){
-			String teste = value+i+".txt";
-			String[] a = new ReadFactory().read(teste);
-			int[] b = new ReadFactory().converts(a);
-			
-			bubble.structure(b);
+
+		switch (input) {
+		case 1:
+			BubbleSort bubble = new BubbleSort();
+			for (int i = 1; i <= 5; i++) {
+				String teste = value + i + ".txt";
+				String[] a = new ReadFactory().read(teste);
+				int[] b = new ReadFactory().converts(a);
+				bubble.structure(b);
+			}
+			bubble.write();
+			break;
+		case 2:
+			SelectionSort selection = new SelectionSort();
+			for (int i = 1; i <= 5; i++) {
+				String teste = value + i + ".txt";
+				String[] a = new ReadFactory().read(teste);
+				int[] b = new ReadFactory().converts(a);
+				selection.structure(b);
+			}
+			selection.write();
+			break;
+		case 3:
+			InsertionSort insertion = new InsertionSort();
+			for (int i = 1; i <= 5; i++) {
+				String teste = value + i + ".txt";
+				String[] a = new ReadFactory().read(teste);
+				int[] b = new ReadFactory().converts(a);
+				insertion.structure(b);
+			}
+			insertion.write();
+			break;
+		case 4:
+			QuickSort quick = new QuickSort();
+			for (int i = 1; i <= 6; i++) {
+				String teste = value + i + ".txt";
+				String[] a = new ReadFactory().read(teste);
+				int[] b = new ReadFactory().converts(a);
+				quick.structure(b);
+			}
+			quick.write();
+			break;
+
 		}
-//		System.out.println(Arrays.toString(bubble.time));
-		bubble.write();
-//		String teste = "orderly1.txt";
-//		new ReadFactory().read(teste);
+		// System.out.println(Arrays.toString(bubble.time));
+
+		// String teste = "orderly1.txt";
+		// new ReadFactory().read(teste);
 
 	}
 }
